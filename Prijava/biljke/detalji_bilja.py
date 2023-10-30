4151a73904d3629ca32516c40c763b503a37c650
JuricaBarta <108483675+JuricaBarta@users.noreply.github.com>
2023-10-30 10:38:31 +0100

    Images
    Adding images in gui_biljke is now working... Need to improve it still, but so far so good.

--- Prijava/biljke/detalji_bilja.py
+++ Prijava/biljke/detalji_bilja.py
@@ -9,12 +9,16 @@ class PlantDetails(ttk.Frame):
         super().__init__(parent)
 
         self.plants = session.query(Plant).all()
-        self.plants_list = self.plants[:]  # Create a list to store all plants
+
+        # Initialize plants_list with the order you want
+        self.plants_list = [plant for plant in self.plants]
+
+        # Initialize the current plant index
         self.current_plant_index = 0
 
-        # configure the grid to have two columns
+        # Configure the grid to have two columns
         self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1) 
+        self.grid_rowconfigure(2, weight=1)
 
         self.plant_image_label = Label(self, image=None)
         self.plant_image_label.grid(row=0, column=1, padx=10, pady=10, sticky="e")
@@ -32,19 +36,36 @@ class PlantDetails(ttk.Frame):
         self.plant_description_label.grid(row=2, column=0, padx=10, pady=10)
 
         button_previous_plant = ttk.Button(self, text="Previous Plant", command=self.previous_plant)
-        button_previous_plant.grid(row=3, column=0, padx=10, pady=10, sticky="w") 
+        button_previous_plant.grid(row=3, column=0, padx=10, pady=10, sticky="w")
 
         button_next_plant = ttk.Button(self, text="Next Plant", command=self.next_plant)
-        button_next_plant.grid(row=3, column=1, padx=10, pady=10, sticky="e")  
+        button_next_plant.grid(row=3, column=1, padx=10, pady=10, sticky="e")
 
         button_open_creator_window = ttk.Button(self, text="Dodaj novu biljku", command=self.open_creator_window)
         button_open_creator_window.grid(row=4, column=1, padx=10, pady=10, sticky="e")
 
-        # display the first plant
+        # Display the first plant
         self.display_plant()
 
     def display_plant(self, current_plant=None, plant_images=None):
         if current_plant:
+            # Display the name and description of the current plant
+            self.plant_name_label.configure(text=current_plant.plant_name)
+
+            descriptions = [
+                desc for desc in [
+                    current_plant.plant_description_one,
+                    current_plant.plant_description_two,
+                    current_plant.plant_description_three,
+                    current_plant.plant_description_four
+                ] if desc is not None
+            ]
+
+            if not any(descriptions):
+                self.plant_description_label.configure(text="No more info about the plant")
+            else:
+                self.plant_description_label.configure(text="\n".join(descriptions))
+
             # Fetch the image path for the current plant from associated PlantImage
             if plant_images:
                 plant_image_path = plant_images[0].image_path 
@@ -57,46 +78,22 @@ class PlantDetails(ttk.Frame):
                     self.plant_image_label.configure(image=plant_image_tk)
                     self.plant_image_label.image = plant_image_tk
                 else:
-                    self.plant_image_label.configure(text="No Image")
-            else:
-                self.plant_image_label.configure(text="No Image")
-
-            # Display the name and description of the current plant
-            self.plant_name_label.configure(text=current_plant.plant_name)
-
-            descriptions = [
-                desc for desc in [
-                    current_plant.plant_description_one,
-                    current_plant.plant_description_two,
-                    current_plant.plant_description_three,
-                    current_plant.plant_description_four
-                ] if desc is not None  
-            ]
-
-            if not any(descriptions):
-                self.plant_description_label.configure(text="No more info about the plant")
-            else:
-                self.plant_description_label.configure(text="\n".join(descriptions))
-
-
-    def add_new_plant(self):
-        name = "New Plant" 
-        image = None  
-        create_plant(name, image)
-
-        new_plant = session.query(Plant).filter_by(plant_name=name).first()
-        self.plants_list.append(new_plant)
+                    self.plant_image_label.configure(image=None)  # Clear the image
+                    self.plant_image_label.configure(text="No image")
 
     def next_plant(self):
         if self.current_plant_index < len(self.plants_list) - 1:
             self.current_plant_index += 1
-            self.display_plant()
-
+        current_plant = self.plants_list[self.current_plant_index]
+        plant_images = current_plant.plant_images  # Fetch the associated images
+        self.display_plant(current_plant, plant_images)
 
     def previous_plant(self):
         if self.current_plant_index > 0:
             self.current_plant_index -= 1
-            self.display_plant()
+        current_plant = self.plants_list[self.current_plant_index]
+        plant_images = current_plant.plant_images  # Fetch the associated images
+        self.display_plant(current_plant, plant_images)
 
     def open_creator_window(self):
         create_new_plant_screen = CreateNewPlantScreen()
--- Prijava/biljke/gui_biljke.py
+++ Prijava/biljke/gui_biljke.py
@@ -1,10 +1,9 @@
 import tkinter as tk
+from tkinter import filedialog
 from database import *
 from crud import *
 from biljke.crud_bilja import CreateNewPlantScreen
 from PIL import Image, ImageTk
-
-
 
 class PlantsScreen(tk.Frame):
     def __init__(self, parent, main_screen):
@@ -24,30 +23,32 @@ class PlantsScreen(tk.Frame):
         self.frame = tk.Frame(self.canvas)
         self.canvas.create_window((0, 0), window=self.frame, anchor='nw')
 
-
         self.plant_labelframes = []
         self.add_plant_button = tk.Button(self.label, text="Dodaj novu biljku", command=self.show_new_plant_screen)
         self.add_plant_button.grid(row=1, column=2, sticky='ne')
 
-        self.refresh_button = tk.Button(self.label, text="Refresh", command=self.update_plant_labelframes)
+        self.refresh_button = tk.Button(self.label, text="Refresh", command=self.create_plant_labelframes)
         self.refresh_button.grid(row=0, column=2, sticky='nw')
 
         self.create_plant_labelframes()
 
         self.frame.bind('<Configure>', self.on_frame_configure)
 
-
     def on_frame_configure(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox('all'))
 
     def switch_to_tab4(self, current_plant):
         tab4 = self.main_screen.notebook.nametowidget(self.main_screen.notebook.tabs()[3])
-        self.main_screen.notebook.select(3)
 
         # Fetch associated PlantImage objects for the selected plant
         plant_images = current_plant.plant_images
 
-        tab4.display_plant(current_plant, plant_images)
+        if plant_images:
+            self.main_screen.notebook.select(3)
+            tab4.display_plant(current_plant, plant_images)
+        else:
+            # Pass the LabelFrame to the upload_and_display_image function
+            self.upload_and_display_image(current_plant, create_plant_image)
 
     def create_plant_labelframes(self):
         plants = session.query(Plant).all()
@@ -59,25 +60,35 @@ class PlantsScreen(tk.Frame):
             labelframe_plant = tk.LabelFrame(self.frame)
             labelframe_plant.grid(row=row, column=column, padx=10, pady=10)
 
-            plant_label = tk.Button(
-                labelframe_plant,
-                command=lambda current_plant=plant: self.switch_to_tab4(current_plant)
-            )
-
             plant_images = plant.plant_images
 
-            if plant_images:
+            # Create a flag to check if the plant image is available
+            has_plant_image = bool(plant_images)
+
+            # Initialize plant_picture as None
+            plant_picture = None
+
+            if has_plant_image:
                 first_plant_image = plant_images[0]
                 image_filename = first_plant_image.image_path
 
                 # Create an instance of your PlantImage class and get the resized image
                 plant_picture = PlantImage(image_filename).open_and_resize()
 
-                if plant_picture:
-                    plant_label.config(image=plant_picture)
-                    plant_label.image = plant_picture 
+            def plant_button_click():
+                if not has_plant_image:
+                    # If no plant image, open upload_and_display_image
+                    self.upload_and_display_image(plant, labelframe_plant)
                 else:
-                    pass
+                    # If plant image is available, switch to tab 4
+                    self.switch_to_tab4(plant)
+
+            # Create the plant label button
+            plant_label = tk.Button(labelframe_plant, command=plant_button_click)
+
+            if plant_picture:
+                plant_label.config(image=plant_picture)
+                plant_label.image = plant_picture
 
             plant_label.grid(row=0, column=0)
 
@@ -85,41 +96,6 @@ class PlantsScreen(tk.Frame):
             plant_name_label.grid(row=1, column=0, pady=10, padx=10)
 
             self.plant_labelframes.append(labelframe_plant)
-
-    def show_new_plant_screen(self):
-        new_plant_screen = CreateNewPlantScreen()
-        new_plant = new_plant_screen.add_buttons()
-
-        if new_plant:
-            for i, labelframe in enumerate(self.plant_labelframes):
-                plant_name_label = labelframe.grid_slaves(row=1, column=0)[0]
-                if not plant_name_label.cget('text'):
-                    self.plant_names[i] = new_plant[0]
-                    self.plant_images[i] = new_plant[1]
-                    self.update_plant_labelframe(i, *new_plant)
-                    break
-
-    def update_plant_labelframe(self, index, plant_name, plant_image):
-        labelframe_plant = self.plant_labelframes[index]
-        plant_label = labelframe_plant.grid_slaves(row=0, column=0)[0]
-        current_image = plant_label.cget('image').filename
-        if plant_image != current_image:
-            plant_picture = PlantImage(plant_image)
-            plant_label.config(image=plant_picture.get_image())
-        plant_name_label = labelframe_plant.grid_slaves(row=1, column=0)[0]
-        plant_name_label.config(text=f"{plant_name} Plant")
-
-    def update_plant_labelframes(self):
-        self.frame.destroy()
-
-        # Recreate the plant labels based on updated plant data
-        self.frame = tk.Frame(self.canvas)
-        self.canvas.create_window((0, 0), window=self.frame, anchor='nw')
-        self.create_plant_labelframes()
-
-        # Update the layout
-        self.canvas.update_idletasks()
-        self.canvas.config(scrollregion=self.canvas.bbox("all"))
 
     def show_new_plant_screen(self):
         new_plant_screen = CreateNewPlantScreen()
@@ -144,8 +120,8 @@ class PlantsScreen(tk.Frame):
             # For existing plants with images, create a button with the existing behavior
             plant_label = tk.Button(labelframe_plant, command=lambda: self.switch_to_tab4())
         else:
-            # Make upload photo button!!!
-            plant_label = tk.Button(labelframe_plant, command=lambda plant_name=plant_name: self.custom_command(plant_name))
+            # Create an "Upload Photo" button
+            plant_label = tk.Button(labelframe_plant, text="Upload Photo", command=lambda: self.upload_and_display_image(labelframe_plant))
 
         if plant_image:
             plant_picture_in = PlantImage(plant_image)
@@ -158,6 +134,24 @@ class PlantsScreen(tk.Frame):
 
         self.plant_labelframes.append(labelframe_plant)
 
+    def upload_and_display_image(self, current_plant, labelframe):
+        # Open a file dialog for image selection
+        f_types = [("Jpg files", "*.jpg"), ("PNG files", "*.png")]
+        filename = filedialog.askopenfilename(filetypes=f_types)
+
+        if filename:
+            # Load the selected image and display it in the label
+            img = Image.open(filename)
+            img = img.resize((150, 200))
+            img = ImageTk.PhotoImage(img)
+            image_label = labelframe.grid_slaves(row=0, column=0)[0]
+            image_label.config(image=img)
+            image_label.image = img
+
+            # Set the flag to indicate that the plant now has an image
+            current_plant.has_plant_image = True
+
+
     def custom_command(self, plant_name):
         print(f"Custom command for {plant_name}")
 
@@ -167,8 +161,22 @@ class PlantsScreen(tk.Frame):
             del self.plant_labelframes[index]
 
     def create_new_labelframe(self):
-        plant_name = "New Plant"  
-        plant_image = None 
-        self.add_plant_labelframe(plant_name, plant_image)
+        plant_name = "New Plant"
+        plant_image = None
 
-    
+        column = len(self.plant_labelframes) % 2
+        row = len(self.plant_labelframes) // 2
+
+        labelframe_plant = tk.LabelFrame(self.frame)
+        labelframe_plant.grid(row=row, column=column, padx=10, pady=10)
+
+        # Correct the function call here
+        plant_label = tk.Button(labelframe_plant, text="Upload Photo", command=lambda: self.upload_and_display_image(labelframe_plant))
+        plant_label.grid(row=0, column=0)
+
+        plant_name_label = tk.Label(labelframe_plant, text=f"{plant_name} Plant")
+        plant_name_label.grid(row=1, column=0, pady=10, padx=10)
+
+        self.plant_labelframes.append(labelframe_plant)
+
+
